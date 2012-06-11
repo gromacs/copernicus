c0260a162ffb3adc194d2ddb59d978819be7216d
Sander Pronk <pronk@cbr.su.se>
2012-06-11 16:35:12 +0200

    Added test files for g_bar function

---
+++ cpc/test/lib/gromacs_bar/runtest.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# check whether all input files will be available:
+if [ ! -e cpc/test/lib/gromacs_bar/lambda00.edr ]; then
+    echo "This test script must be run from within the copernicus base directory"
+    exit 1
+fi
+
+# start the project
+./cpcc start test
+# import the gromacs module with grompp and mdrun functions
+./cpcc import gromacs
+# add the grompp and mdrun function instances
+./cpcc instance gromacs::g_bar bar
+# activate the function instance
+./cpcc activate
+
+
+# start a transaction: all the 'set' and 'connect' commands following this
+# will be executed as one atomic operation upon the cpcc commit command.
+./cpcc transact
+
+
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda00.edr
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda01.edr
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda02.edr
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda03.edr
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda04.edr
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda05.edr
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda06.edr
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda07.edr
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda08.edr
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda09.edr
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda10.edr
+./cpcc set-file bar:in.edr[+] cpc/test/lib/gromacs_bar/lambda11.edr
+
+
+
+# and commit this set of updates
+./cpcc commit
+
+
+./cpcc get bar:out.dG
