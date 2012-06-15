bb1c980ef0e0ece6afde5d8a7ddc146d399956a1
Sander Pronk <pronk@cbr.su.se>
2012-06-15 14:33:30 +0200

    FE solvation function.

--- cpc/test/lib/fe/runtest.sh
+++ cpc/test/lib/fe/runtest.sh
@@ -11,8 +11,7 @@ fi
 # import the gromacs module with grompp and mdrun functions
 ./cpcc import fe
 # add the grompp and mdrun function instances
-./cpcc instance fe::fe_init fe_init
-./cpcc instance fe::fe_iteration fe_iteration
+./cpcc instance fe::fe_solvation fe
 # activate the function instance
 ./cpcc activate
 
@@ -21,27 +20,28 @@ fi
 # will be executed as one atomic operation upon the cpcc commit command.
 ./cpcc transact
 
-./cpcc set-file fe_init:in.grompp.top cpc/test/lib/fe_init/topol.top
-./cpcc set-file fe_init:in.grompp.include[0]  cpc/test/lib/fe_init/ana.itp
-./cpcc set-file fe_init:in.grompp.mdp cpc/test/lib/fe_init/grompp.mdp
+./cpcc set-file fe:in.grompp.top cpc/test/lib/fe/topol.top
+./cpcc set-file fe:in.grompp.include[0]  cpc/test/lib/fe/ana.itp
+./cpcc set-file fe:in.grompp.mdp cpc/test/lib/fe/grompp.mdp
 
-./cpcc set-file fe_init:in.conf cpc/test/lib/fe_init/conf.gro
+./cpcc set-file fe:in.conf cpc/test/lib/fe/conf.gro
 
-./cpcc set fe_init:in.molecule_name ethanol
+./cpcc set fe:in.molecule_name  ethanol
+./cpcc set fe:in.solvation_relaxation_time 1000
+./cpcc set fe:in.precision 1
 
-./cpcc set fe_init:in.n_lambdas 10
-./cpcc set fe_init:in.nsteps 2000
-
-./cpcc set fe_init:in.a vdwq
-./cpcc set fe_init:in.b vdw
-
-./cpcc connect fe_init:out.path fe_iteration:in.path
-./cpcc connect fe_init:out.resources fe_iteration:in.resources
-./cpcc connect fe_init:out.grompp fe_iteration:in.grompp
+#./cpcc set fe_init:in.n_lambdas 10
+#
+#./cpcc set fe_init:in.a vdwq
+#./cpcc set fe_init:in.b vdw
+#
+#./cpcc connect fe_init:out.path fe_iteration:in.path
+#./cpcc connect fe_init:out.resources fe_iteration:in.resources
+#./cpcc connect fe_init:out.mdp fe_iteration:in.grompp.mdp
 #./cpcc set-file fe_iteration:in.grompp.top cpc/test/lib/fe_init/topol.top
 #./cpcc set-file fe_iteration:in.grompp.include[0]  cpc/test/lib/fe_init/ana.itp
-#./cpcc set fe_iteration:in.molecule_name ethanol
-./cpcc set fe_iteration:in.nsteps 1000
+#./cpcc set fe_iteration:in.coupled_mol ethanol
+#./cpcc set fe_iteration:in.nsteps 1000
 
 # and commit this set of updates
 ./cpcc commit
