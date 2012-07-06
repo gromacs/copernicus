788b40318a21d34de73811214a7d8928877616d9
Sander Pronk <pronk@cbr.su.se>
2012-07-06 13:42:48 +0200

    FE Fixes
    - Stop condition
    - More informative output

--- cpc/lib/fe/fe_decouple.py
+++ cpc/lib/fe/fe_decouple.py
@@ -39,37 +39,107 @@ from cpc.dataflow import ArrayValue
 class FEError(cpc.dataflow.ApplicationError):
     pass
 
-def calcAvg(name, inp, out):
-    """Calculate an average of measurement values of subnet input array 
-        'name'
-         
-         returns a tuple of (value, error, N)"""
-    N=0
-    sumVal=0.
-    sumErr=0.
-    dgArray=inp.getSubnetInput(name) 
-    if dgArray is not None: 
-        # we ignore 0 because that's equilibration
-        for i in range(1, len(dgArray)):
-            #sys.stderr.write('%s[%d]\n'%(name, i))
-            subval=inp.getSubnetInput('%s[%d]'%(name,i))
-            #sys.stderr.write('%s[%d]: %g/%d +/- %g/%d\n'%
-            #                 (name, i, sumVal, N, sumErr, N))
-            if subval is not None:
-                val=inp.getSubnetInput('%s[%d].value'%(name, i))
-                err=inp.getSubnetInput('%s[%d].error'%(name, i))
-                if val is not None and err is not None:
-                    sys.stderr.write('%s[%d]: %g +/- %g\n'%(name, i, val, err))
-                    sumVal += val
-                    sumErr += err*err
-                    N+=1
-    if N>0:
-        sys.stderr.write('%s: %g +/- %g (N=%d)\n'%
-                         (name, sumVal/N, math.sqrt(sumErr/N), N)) 
-        return ( sumVal/N, math.sqrt(sumErr/N), N)
-    else:
-        sys.stderr.write('N=0\n')
-        return (0., 0., 0)
+class partialRes(object):
+    """Holds partial results."""
+    def __init__(self, name, inp):
+        """name = name of input, 
+           inp = the function input object"""
+        self.name=name
+        self.inp=inp
+        self.res=[]
+        dgArray=inp.getSubnetInput(name) 
+        if dgArray is not None: 
+            # we ignore 0 because that's equilibration
+            for i in range(1, len(dgArray)):
+                #sys.stderr.write('%s[%d]\n'%(name, i))
+                subval=inp.getSubnetInput('%s[%d]'%(name,i))
+                if subval is not None:
+                    val=inp.getSubnetInput('%s[%d].value'%(name, i))
+                    err=inp.getSubnetInput('%s[%d].error'%(name, i))
+                    if val is not None and err is not None:
+                        self.res.append( (val, err) )
+        self._calcAvg()
+
+    def _calcAvg(self):
+        """Calculates an average of measurement values of subnet input array 
+            'name' """
+        self.mean=None
+        self.err=None
+        if len(self.res)<=0:
+            return
+        sumVal=0.
+        sumInvErr=0.
+        for res in self.res:
+            val=res[0]
+            err=res[1]
+            sys.stderr.write('%g +/- %g\n'%(val, err))
+            # add the weighted value
+            sumVal += val/(err*err)
+            # and keep track of the sum of weights
+            sumInvErr += 1/(err*err)
+        if len(self.res)>0:
+            self.mean=sumVal/sumInvErr
+            self.err=math.sqrt(1/sumInvErr)
+            sys.stderr.write('Avg: %g +/- %g (N=%d)\n'%(self.mean, self.err, 
+                                                       len(self.res))) 
+
+    def getAvg(self):
+        """Calculates the weighted average of measurement values"""
+        return self.mean
+
+    def getErr(self):
+        """Calculates the error estimate on the weighted average of measurement 
+            values"""
+        return self.err
+
+    def getRes(self):
+        """Returns the array with partial results (tuples of value and error)"""
+        return self.res
+
+    def getN(self):
+        """Returns the number of partial results."""
+        return len(self.res)
+
+    def updateOutput(self, pers, out, outputName, index, desc, pathInputName):
+        """Update a partial output with results
+           pers =  the persistence object
+           out = the function output object
+           outputName = the name of the partial results output item
+           index = the main index to use.
+           desc = a string describing the part of the free energy this is for
+           pathInputName = the name of the fe_path subnetInput item
+           returns: True if values have changed"""
+        persName="%s_handled"%self.name
+        Nhandled=pers.get(persName)
+        if Nhandled is None:
+            Nhandled=0
+            out.setOut('%s[%d].desc'%(outputName, index), StringValue(desc))
+        changedValues=False
+        N=len(self.res)
+        if N>0:
+            # TODO: expand this when updated tags propagate correctly
+            while Nhandled<N:
+                changedValues=True
+                out.setOut('%s[%d].parts[%d].value'%(outputName, index, 
+                                                     Nhandled),
+                           FloatValue(self.res[Nhandled][0]))
+                out.setOut('%s[%d].parts[%d].error'%(outputName, index, 
+                                                     Nhandled),
+                           FloatValue(self.res[Nhandled][1]))
+                # do this only once per loop:
+                if Nhandled == N-1:
+                    out.setOut('%s[%d].average.value'%(outputName, index),
+                               FloatValue(self.getAvg()))
+                    out.setOut('%s[%d].average.error'%(outputName, index),
+                               FloatValue(self.getErr()))
+                    # set the fe path
+                    out.setOut('%s[%d].path'%(outputName, index),
+                               self.inp.getSubnetInputValue("%s[%d]"%
+                                                            (pathInputName, 
+                                                             Nhandled)) )
+                Nhandled+=1
+        pers.set(persName, Nhandled)
+        return changedValues
 
 def addIteration(name, i, inp, out):
     """Add one fe calc iteration."""
@@ -93,6 +163,7 @@ def addIteration(name, i, inp, out):
         #                  'iter_q_%d:in.lambdas'%i )
     # connect the outputs
     out.addConnection('%s:out.dG'%iname, 'self:sub_in.dG_%s_array[%d]'%(name,i))
+    out.addConnection('%s:out.path'%iname, 'self:sub_in.path_%s[%d]'%(name,i))
 
 def decouple(inp, out, relaxation_time, mult):
     pers=cpc.dataflow.Persistence(os.path.join(inp.persistentDir,
@@ -147,65 +218,42 @@ def decouple(inp, out, relaxation_time, mult):
         addIteration("lj", 0, inp, out)
         addIteration("lj", 1, inp, out)
 
-    dgOutputsHandled=pers.get('dg_outputs_handled')
-    # read in the dG inputs.
-    # whether the q (0) or lj (1) parts have changed
-    changedValues=[False, False]
-    # the total value+error for q (0) and lj (1)
-    totVals=[ (None, None), (None, None)]
-    end=False
-    # q first
-    (val, err, N) = calcAvg('dG_q_array', inp, out)
-    dg_q_handled=pers.get('dg_q_handled')
-    if N>0:
-        totVals[0]= (val, err) 
-        if dg_q_handled is None or dg_q_handled<N:
-            dg_q_handled=N
-            changedValues[0]=True
-    else:
-        dg_q_handled=0
-    pers.set('dg_q_handled', dg_q_handled)
-    # lj next first
-    (val, err, N) = calcAvg('dG_lj_array', inp, out)
-    dg_l_handled=pers.get('dg_lj_handled')
-    if N>0:
-        totVals[1]=(val, err)
-        if dg_l_handled is None or dg_q_handled<N:
-            dg_l_handled=N
-            changedValues[1]=True
-    pers.set('dg_lj_handled', dg_l_handled)
+    res_q=partialRes('dG_q_array', inp)
+    changed_q=res_q.updateOutput(pers, out, 'partial_results', 0, 
+                                 'electrostatics decoupling', 'path_q')
+    # lj next 
+    res_lj=partialRes('dG_lj_array', inp)
+    changed_lj=res_lj.updateOutput(pers, out, 'partial_results', 1,
+                                   'Lennard-Jones decoupling', 'path_lj')
 
     precision=inp.getInput('precision')
     if precision is None:
         precision = 1
-    if changedValues[0] or changedValues[1]:
-        if not (totVals[0][0] is None or totVals[1][0] is None):  
-            # len(totVals) == 2:
-            totVal=0.
-            totErr=0.
-            for val, err in totVals:
-                totVal += val
-                totErr += err*err
-            #totVal /= len(totVals)
-            totErr = math.sqrt(totErr/len(totVals))
+    if changed_q or changed_lj:
+        totErr = 2*precision
+        if not (res_lj.getAvg() is None or res_q.getAvg() is None):
+            # update the totals
+            totVal = res_lj.getAvg() + res_q.getAvg()
+            totErr = math.sqrt( (res_lj.getErr()*res_lj.getErr() + 
+                                 res_q.getErr()*res_q.getErr())/2. )
             out.setOut('delta_f.value', FloatValue(mult*totVal))
             out.setOut('delta_f.error', FloatValue(totErr))
-            #precision=inp.getInput('precision')
-            #if totErr > precision:
-                #addIteration(inp, out, nruns)
-            #    nruns += 1
         # now add iterations if the error is more than half the desired error
         # (in which case it's better to add iterations to the other half)
-        if changedValues[0]:
-            if totVals[0][1] > precision/2:
+        if totErr > precision:
+            # we may need to start new runs. We use precision/2 as a cutoff 
+            # for each of the halves because after that is reached it makes
+            # more sense to sample the other side more thoroughly.
+            if changed_q and res_q.getErr() > precision/2:
                 sys.stderr.write('Adding another q iteration\n')
                 addIteration("q", nruns_q, inp, out)
                 nruns_q += 1
-        if changedValues[1]:
-            if totVals[1][1] > precision/2:
-                sys.stderr.write('Adding another lj iteration\n')
+            if changed_lj and res_lj.getErr() > precision/2:
+                sys.stderr.write('Adding another q iteration\n')
                 addIteration("lj", nruns_lj, inp, out)
                 nruns_lj += 1
+        else:
+            sys.stderr.write("Desired precision reached.\n")
         
     pers.set('nruns_q', nruns_q)
     pers.set('nruns_lj', nruns_lj)
