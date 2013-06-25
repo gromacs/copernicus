22b21f598d1b163f8d4bce046dd4a9453e02581c
Magnus Lundborg <lundborg.magnus@gmail.com>
2013-06-25 11:07:02 +0200

    Alternative documentation generator (sphinx).
    This is just for testing atm.
    
    Change-Id: I248168ae2e32cfb3f31521381ad31a93e294f650

---
+++ genDoc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd $CPC_HOME
+PYTHONPATH=$CPC_HOME
+sphinx-apidoc -F -H Copernicus -A 'Sander Pronk, Iman Pouya, Per Larsson, Magnus Lundborg, Erik Lindahl, Per Kasson, Martin Andersson' -V 2.0 -o doc/api cpc
+cd doc/api
+make html
