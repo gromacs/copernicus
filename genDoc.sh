e4af7620bb5203adf9db59514b85c48637e6da3f
Patrik Falkman <patrikf@gmail.com>
2013-06-27 15:39:08 +0200

    Added Patrik Falkman to authors list
    Change-Id: Ic19c8e0ef607774efddfc3a94483e09e01469f6e

--- genDoc.sh
+++ genDoc.sh
@@ -2,6 +2,6 @@
 
 cd $CPC_HOME
 PYTHONPATH=$CPC_HOME
-sphinx-apidoc -F -H Copernicus -A 'Sander Pronk, Iman Pouya, Per Larsson, Magnus Lundborg, Erik Lindahl, Per Kasson, Martin Andersson' -V 2.0 -o doc/api cpc
+sphinx-apidoc -F -H Copernicus -A 'Sander Pronk, Iman Pouya, Per Larsson, Magnus Lundborg, Erik Lindahl, Per Kasson, Patrik Falkman, Martin Andersson' -V 2.0 -o doc/api cpc
 cd doc/api
 make html
