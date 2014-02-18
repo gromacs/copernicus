91139095084ee7b3f27e13d2e366b729435786c6
Iman Pouya <iman.pouya@gmail.com>
2014-02-18 14:07:47 +0100

    Release script
    Given a version number this script will
    1. check out a new branch
    2. change the version number in cpc/util/version.py
    3. commit the change
    4. create an archive for release
    5. push the tag to refs/tags
    
    if the version number exists in refs/tags the script will not do the
    above 5 steps but only create an archive
    
    Change-Id: I67567f8f29e2733b1b94b6dc2d4ed7a0218ace51

--- cpc/util/version.py
+++ cpc/util/version.py
@@ -16,6 +16,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = "1.1.0-dev"
+__version__="b-1"
 
 
