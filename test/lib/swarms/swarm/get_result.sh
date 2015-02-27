ae761b6e9c210c61b2893f1d095f52d7f4bdd9e5
Bjorn Wesen <bjorn.wesen@gmail.com>
2015-02-27 14:11:32 +0100

    Updated to extract 16 stringpoints

--- test/lib/swarms/swarm/get_result.sh
+++ test/lib/swarms/swarm/get_result.sh
@@ -9,16 +9,18 @@ for ITER in {0..79}
 do
     # Points to get data from (including start/end)
     # Note: check the -eq 0 and -eq 17 below too
-    for MIDX in {0..17}
+    for MIDX in {0..15}
     do
 
-        if [ $MIDX -eq 0 ]; then
-            CPCVAR="run:iter$ITER:in.start_conf"
-        elif [ $MIDX -eq 17 ]; then
-            CPCVAR="run:iter$ITER:in.end_conf"
-        else
-            CPCVAR="run:iter$ITER:equilibration:out.confs[$((MIDX-1))]"
-        fi
+#        if [ $MIDX -eq 0 ]; then
+#            CPCVAR="run:iter$ITER:in.start_conf"
+#        elif [ $MIDX -eq 17 ]; then
+#            CPCVAR="run:iter$ITER:in.end_conf"
+#        else
+#            CPCVAR="run:iter$ITER:equilibration:out.confs[$((MIDX-1))]"
+#        fi
+
+        CPCVAR="run:iter$ITER:equilibration:out.confs[$MIDX]"
 
         # Get a conf from all points including start/end
         cpcc getf -f "iter${ITER}_conf$MIDX.gro" $CPCVAR
