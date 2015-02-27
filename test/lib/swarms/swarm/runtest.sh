971767489c9af86789ed41e1412ab23fd8b68e9d
Bjorn Wesen <bjorn.wesen@gmail.com>
2015-02-27 14:15:50 +0100

    Clean up and add some comments. Set the fix_endpoints = 0 option to let the endpoints drift, this is required to match the published articles about the alanine dipeptide and the string method.

--- test/lib/swarms/swarm/runtest.sh
+++ test/lib/swarms/swarm/runtest.sh
@@ -30,22 +30,32 @@ fi
 # declare minimization settings
 ./cpcc  set-file run:in.minim_grompp.mdp test/lib/swarms/swarm/minim.mdp
 ./cpcc  set-file run:in.minim_grompp.top test/lib/swarms/swarm/topol.top
-./cpcc  set-file run:in.minim_grompp.ndx test/lib/swarms/swarm/index.ndx
+./cpcc  set run:in.minim_restrforce 4000.0
 ./cpcc  set run:in.em_tolerance 100
 
 # declare the thermalization settings
-./cpcc  set-file run:in.therm_grompp.mdp test/lib/swarms/swarm/grompp.mdp
+./cpcc  set-file run:in.therm_grompp.mdp test/lib/swarms/swarm/therm.mdp
 ./cpcc  set-file run:in.therm_grompp.top test/lib/swarms/swarm/topol.top
-./cpcc  set-file run:in.therm_grompp.ndx test/lib/swarms/swarm/index.ndx
+./cpcc  set run:in.therm_restrforce 4000.0
 
 # declare restrained run and swarm settings
-./cpcc  set-file run:in.equi_grompp.mdp test/lib/swarms/swarm/grompp.mdp
+./cpcc  set-file run:in.equi_grompp.mdp test/lib/swarms/swarm/run.mdp
 ./cpcc  set-file run:in.equi_grompp.top test/lib/swarms/swarm/topol.top
-./cpcc  set-file run:in.equi_grompp.ndx test/lib/swarms/swarm/index.ndx
-./cpcc  set run:in.restrained_steps 5000
-./cpcc  set run:in.swarm_steps 50
-./cpcc  set run:in.Nswarms 10
-./cpcc  set run:in.Ninterpolants 5
+./cpcc  set run:in.equil_restrforce 4000.0
+
+./cpcc  set run:in.restrained_steps 20000
+./cpcc  set run:in.swarm_steps 15
+
+# This is the number of swarm configurations to launch for each stringpoint, so it
+# amplifies the work needed quite much if increased. For a demo, 10-20 is enough here
+# but to get a smooth curve as a result, it should be higher, like 50-100.
+
+./cpcc  set run:in.Nswarms 20
+
+# The number of stringpoints to use, 10-16 are definitely enough for the alanine dipeptide
+# demo, but for more complex transitions you should probably use more.
+
+./cpcc  set run:in.Ninterpolants 16
 
 # the residue (collective variable) index
 ./cpcc  set-file run:in.cv_index test/lib/swarms/swarm/res.ndx
@@ -56,10 +66,18 @@ fi
 # a topology for writing basic restraints
 ./cpcc  set-file run:in.top test/lib/swarms/swarm/topol.top
 
+# Allow the start/end points to drift just like the rest of the stringpoints
+./cpcc  set run:in.fix_endpoints 0
+
 ./cpcc  set run:in.Nchains 1
-./cpcc  set run:in.Niterations 3
+
+# Number of iterations - for quick testing, set this to a lower number
+# For the complete demo, 50-80 iterations are required when using the other settings here
+
+#./cpcc  set run:in.Niterations 10
+./cpcc  set run:in.Niterations 80
+
 # and commit this set of updates
 ./cpcc  commit
 
-#./cpcc  get tune:out.resources
-./cpcc  get run:out
+
