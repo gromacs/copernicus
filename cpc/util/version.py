39f453c10f72ade825b503ec677a37a7b27dbf2f
Iman Pouya <iman.pouya@gmail.com>
2015-08-11 10:05:53 +0200

    setting version 2.1.4

--- cpc/util/version.py
+++ cpc/util/version.py
@@ -16,6 +16,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__="2.1.1"
+__version__="2.1.4"
 
 
